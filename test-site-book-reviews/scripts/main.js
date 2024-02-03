e07d4dab8c7178f729a796e4ff97a0366a92631e
perryvcode <158760875+perryvcode@users.noreply.github.com>
2024-02-03 11:39:25 -0500

    Update main.js

--- test-site-book-reviews/scripts/main.js
+++ test-site-book-reviews/scripts/main.js
@@ -2,10 +2,10 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "test-site-book-reviews/images/delillo-cosmopolis.jpg") {
-    myImage.setAttribute("src", "test-site-book-reviews/images/delillo-cosmopolis2.jpg");
+  if (mySrc === "https://github.com/perryvcode/perryvcode.github.io/blob/main/test-site-book-reviews/images/delillo-cosmopolis.jpg") {
+    myImage.setAttribute("src", "https://github.com/perryvcode/perryvcode.github.io/blob/main/test-site-book-reviews/images/delillo-cosmopolis2.jpg");
   } else {
-    myImage.setAttribute("src", "test-site-book-reviews/images/delillo-cosmopolis.jpg");
+    myImage.setAttribute("src", "https://github.com/perryvcode/perryvcode.github.io/blob/main/test-site-book-reviews/images/delillo-cosmopolis.jpg");
   }
 };
 let myButton = document.querySelector("button");

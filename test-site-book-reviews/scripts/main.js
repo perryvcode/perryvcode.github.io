d921587a6022cd9393ca38d08c2d5aad9804fc5b
perryvcode <158760875+perryvcode@users.noreply.github.com>
2024-02-03 11:54:28 -0500

    Update main.js

--- test-site-book-reviews/scripts/main.js
+++ test-site-book-reviews/scripts/main.js
@@ -3,9 +3,9 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "/images/delillo-cosmopolis.jpg") {
-    myImage.setAttribute("src", "/images/delillo-cosmopolis2.jpg");
+    myImage.setAttribute("src", "images/delillo-cosmopolis2.jpg");
   } else {
-    myImage.setAttribute("src", "/images/delillo-cosmopolis.jpg");
+    myImage.setAttribute("src", "images/delillo-cosmopolis.jpg");
   }
 };
 let myButton = document.querySelector("button");
